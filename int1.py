3879e7d98e44e8f216a2c6978a25c166c9431512
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 13:27:30 +0600

    int 1

---
+++ int1.py
@@ -0,0 +1,20 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> a=input()
+5
+>>> a=int(a)
+>>> b=input()
+10
+>>> b=int(b)
+>>> a*b
+50
+>>> a+b
+15
+>>> a-b
+-5
+>>> a/b
+0.5
+>>> a%b
+5
+>>> 
