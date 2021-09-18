babdfb2c6031cbea87bd7fc074bb5373084f467f
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 14:16:45 +0600

    man ber kora oneline statement

---
+++ man ber kora.py
@@ -0,0 +1,15 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a,b=input().split()
+10 20
+>>> a
+'10'
+>>> b
+'20'
+>>> a
+'10'
+>>> type(a)
+<class 'str'>
+>>> type(b)
+<class 'str'>
+>>> 
