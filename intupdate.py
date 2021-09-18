e73bd9a84ecdcab11ebe760e8bc2d2cec70889c0
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-18 13:48:38 +0600

    int update

---
+++ intupdate.py
@@ -0,0 +1,17 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input())
+50
+>>> b=int(input())
+20
+>>> a+b
+70
+>>> a-b
+30
+>>> a*b
+1000
+>>> a/b
+2.5
+>>> a%b
+10
+>>> 
