58cccabf7b07f872310e3ddc9102b9d193cb1390
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-19 21:11:49 +0600

    compiresion operator

---
+++ compaireson operator.py
@@ -0,0 +1,22 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #==,!=,>,<,>=,<= agula shob compireson operator
+>>> 2==2
+True
+>>> 1<2
+True
+>>> 1!=1
+False
+>>> 1!=2
+True
+>>> True
+True
+>>> 2>5
+False
+>>> 1>0
+True
+>>> 2>=1
+True
+>>> 1<=2
+True
+>>> 
