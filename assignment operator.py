c739c21c7a42f8cf752cdf8a0a1640b1959efc4c
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-19 21:41:46 +0600

    assignment operator

---
+++ assignment operator.py
@@ -0,0 +1,19 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=4
+>>> a+=2
+>>> a
+6
+>>> a-=2
+>>> a
+4
+>>> a*=2
+>>> a
+8
+>>> a/=2
+>>> a
+4.0
+>>> a%=2
+>>> a
+0.0
+>>> #=,+=,-=,/=,*=,%= agula shob assignment operator
