a95d92d0c1df6665632111b48216f7ed7c8dd16a
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-20 21:12:17 +0600

    identity operator

---
+++ identity operator.py
@@ -0,0 +1,16 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # is -- is not --identity operator
+>>> a=1
+>>> b=1
+>>> a is b
+True
+>>> b is a
+True
+>>> a=6000
+>>> b=6000
+>>> a is b
+False
+>>> b is a
+False
+>>> 
