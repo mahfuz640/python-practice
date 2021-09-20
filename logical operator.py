a3e188410afe0b42dcafd40a6acaccd0c2fead8a
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-20 19:13:54 +0600

    logical operator

---
+++ logical operator.py
@@ -0,0 +1,16 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #and,or,not logical operator
+>>> 2==2 and 3==3
+True
+>>> 2==2 and 4==3
+False
+>>> 2==2 or 3==4
+True
+>>> 2==5 or 3 ==4
+False
+>>> 2==5
+False
+>>> not 2 ==5
+True
+>>> 
