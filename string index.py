ce48f8cebd99b6fa9d299882c909f97817e35b80
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-22 02:14:53 +0600

    string position

---
+++ string index.py
@@ -0,0 +1,19 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> """array in index position gula akotre and alada alada kore ber kora ba akotre ber kora [2,5]position a jara ase tara bade ader majhe jajara ase tader k e pawa jabe but 2,5 ar position a jara ase tader k kintu output a pawa jabe na"""
+'array in index position gula akotre and alada alada kore ber kora ba akotre ber kora [2,5]position a jara ase tara bade ader majhe jajara ase tader k e pawa jabe but 2,5 ar position a jara ase tader k kintu output a pawa jabe na'
+>>> 
+>>> a='Bangladesh'
+>>> a=[2,5]
+>>> a
+[2, 5]
+>>> 
+>>> a='Bangladesh'
+>>> a[2,5]
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a[2,5]
+TypeError: string indices must be integers
+>>> a[2:5]
+'ngl'
+>>> 
