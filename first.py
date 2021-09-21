2f76402e83501b4ebb5529d7f1fc39d7224ee8e7
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-21 22:40:03 +0600

    string position

--- first.py
+++ first.py
@@ -1,1 +1,1 @@
-print('hello vs');
+print('Bangladesh');
---
+++ stringprosition.py
@@ -0,0 +1,18 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a='bangla'
+>>> a
+'bangla'
+>>> a[0]
+'b'
+>>> a[1]
+'a'
+>>> a[2]
+'n'
+>>> a[3]
+'g'
+>>> a[4]
+'l'
+>>> a[5]
+'a'
+>>> 
