b16e82dcdafedb519f5acdbe6d307ecc7a5470ae
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-22 12:10:33 +0600

    string formating

---
+++ string formating.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=Banlgadesh
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a=Banlgadesh
+NameError: name 'Banlgadesh' is not defined
+>>> a='Bangladesh'
+>>> print(a)
+Bangladesh
+>>> #string formating
