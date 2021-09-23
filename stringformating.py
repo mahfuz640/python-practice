a9ffb77d46b2da074061cd0457286f7019dae93f
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-23 20:52:12 +0600

    string formating

---
+++ stringformating.py
@@ -0,0 +1,18 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a='bangla'
+>>> print (a)
+bangla
+>>> print('My favorit language is :',a);
+My favorit language is : bangla
+>>> 
+>>> 
+>>> #amr chaile akhane string ar jonno alada sign use korte pari
+>>> print('what is our Mother Language:%s' %a);
+what is our Mother Language:bangla
+>>> #%d use hoi --intger type ar jonno
+>>> #%f use hoi -- float type number ar jonmn
+>>> #%s use hoi -- string type ar jonno
+>>> print('what is our Mother Language:%s' a);
+SyntaxError: invalid syntax
+>>> 
