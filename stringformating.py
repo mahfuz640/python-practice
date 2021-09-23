1e69c1cc6d6c796cc5cbdb44040699073866d1e5
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-23 21:29:39 +0600

    sting formating

--- stringformating.py
+++ stringformating.py
@@ -15,4 +15,21 @@ what is our Mother Language:bangla
 >>> #%s use hoi -- string type ar jonno
 >>> print('what is our Mother Language:%s' a);
 SyntaxError: invalid syntax
+>>> #number string
+>>> number =4563296.3655421
+>>> print('number')
+number
+>>> print(number)
+4563296.3655421
+>>> print('%.2f'%number)#% ar por . diye joto number dibo doshomic ar por toto ghor nibe.
+4563296.37
+>>> 
+>>> 
+>>> 
+>>> a=input()
+bangla
+>>> b=input()
+English
+>>> print('My favorit language are',a,'and',b)
+My favorit language are bangla and English
 >>> 
