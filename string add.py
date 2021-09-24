6aa6908b5d721077b98eb44a7980d31f0c64a46c
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-24 18:08:53 +0600

    string added

---
+++ string add.py
@@ -0,0 +1,13 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a='Bangla'
+>>> a
+'Bangla'
+>>> b='desh'
+>>> b
+'desh'
+>>> #akhon ay alada string duita k amr jodi akotre likhte chai tahole niche lokkho kori
+>>> a+b
+'Bangladesh'
+>>> #akhon amra 3ta variable nibo abong tader add korbo
+>>> 
