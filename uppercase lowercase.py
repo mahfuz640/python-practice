9ef484c3ac2d61faffd9bbb73afd9109d3df54bd
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-26 00:12:56 +0600

    uppercase and lowercase

---
+++ uppercase lowercase.py
@@ -0,0 +1,16 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #amra bangla b take type na kore capitalize function use kore big hand B banabo
+>>> a='banlga'
+>>> a.capitalize()
+'Banlga'
+>>> amra shob letter gulo k uppercase a convert korar jonno boro hat a
+SyntaxError: invalid syntax
+>>> #upper()function use korbo
+>>> a.upper()
+'BANLGA'
+>>> #amra kono akta sentence ar potita word ar 1st word ta uppercase korte chaile amder title() function use korte hobe
+>>> b='my favorit person my father'
+>>> b.title()
+'My Favorit Person My Father'
+>>> 
