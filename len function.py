0f26550488222f983bb67461300c2639dd92c6ef
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-26 23:26:43 +0600

    len function

--- len function.py
+++ len function.py
@@ -1,3 +1,3 @@
 Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+>>> ##amra jokhon kono string ar letter count kore jante chai tokhon amr a chaile len () function use korte pari
