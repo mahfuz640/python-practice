fd5617c1947490c143347839bbd109482175c082
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-26 23:30:53 +0600

    count function

---
+++ count function.py
@@ -0,0 +1,4 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #amra jodi jante chai je akta string a aki letter koibar ase ta count kore ber kora ami
+>>> a=
