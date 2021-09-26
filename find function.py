46b348bc4e0cf281c7216a940c012f93c587cab4
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-27 00:02:53 +0600

    index function

---
+++ find function.py
@@ -0,0 +1,20 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> kono akta letter string ar kon jagai ase ta amra index ar serial number dekhe khuje ber korte parbo find() function diye
+SyntaxError: invalid syntax
+>>> sen="how can a clam in a clean cream can?"
+>>> sen.find('c')
+4
+>>> sen.find('c',5,9)
+-1
+>>> sen.find('c',5)
+10
+>>> #same kaz ar jonno amra index funtion use korte pari
+>>> a="Banladesh is my country"
+>>> a.index(@@)
+SyntaxError: invalid syntax
+>>> a.index("a")
+1
+>>> a.index("a",1,7)
+1
+>>> 
