8bc5a0e47f43f0c51e31f2f1463ac3bf99f5bb91
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-26 23:33:09 +0600

    count function

--- count function.py
+++ count function.py
@@ -1,4 +1,14 @@
 Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> #amra jodi jante chai je akta string a aki letter koibar ase ta count kore ber kora ami
->>> a=
+>>> a='Banlgadesh'
+>>> a
+'Banlgadesh'
+>>> count('a')
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    count('a')
+NameError: name 'count' is not defined
+>>> a.count('a')
+2
+>>> 
