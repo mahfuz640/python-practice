e99d11882468f836a27918f48be0539397bb99ad
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-28 22:50:24 +0600

    update

--- katakuti.py
+++ katakuti.py
@@ -1,4 +1,8 @@
 Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> #amra kono string ar chracter ar jaiga chaile change korte pari ba pori borton korte pari\
->>> #amra akhane replace () function ar 
+>>> #amra akhane replace () function ar
+>>> sen='How can a clam cram in a clean cream can'
+>>> sen.replace('c','d')
+'How dan a dlam dram in a dlean dream dan'
+>>> 
