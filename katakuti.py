1d554f2fe5624ef2e4450938e85b295ebe5a7896
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-28 20:50:57 +0600

    replace

---
+++ katakuti.py
@@ -0,0 +1,4 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #amra kono string ar chracter ar jaiga chaile change korte pari ba pori borton korte pari\
+>>> #amra akhane replace () function ar 
