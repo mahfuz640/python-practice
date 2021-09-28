d0f9f37c6e18e22c17df83386588664f820c889b
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-09-29 00:06:13 +0600

    update

--- katakuti.py
+++ katakuti.py
@@ -5,4 +5,18 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> sen='How can a clam cram in a clean cream can'
 >>> sen.replace('c','d')
 'How dan a dlam dram in a dlean dream dan'
+>>> #akhon amra ? sign k e vanish kore dibo
+>>> sen='How can a clam cram in a clean cream can?'
+>>> sen.replace('?','')
+'How can a clam cram in a clean cream can'
+>>> #ata amara alada arekta function diyeo korte pari
+>>> #sheta holo strip() function
+>>> sentace='How can a clam cram in a clean cream can?'
+>>> sentace.sprip('?')
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    sentace.sprip('?')
+AttributeError: 'str' object has no attribute 'sprip'
+>>> sentace.strip('?')
+'How can a clam cram in a clean cream can'
 >>> 
