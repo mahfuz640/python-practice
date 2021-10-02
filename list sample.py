27a3aa7cfee3c22ee7a7f66aaec2fccd6d09ba5d
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-02 21:57:17 +0600

    list sample]

---
+++ list sample.py
@@ -0,0 +1,13 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=[]
+>>> a
+[]
+>>> type(a)
+<class 'list'>
+>>> a=['onion','potato','ginger','cucumber']
+>>> a
+['onion', 'potato', 'ginger', 'cucumber']
+>>> type(a)
+<class 'list'>
+>>> 
