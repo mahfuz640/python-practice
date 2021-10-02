6eb8c915cb20a84c02a1b417bcad5ff86552c907
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-02 22:40:54 +0600

    update

---
+++ access list.py
@@ -0,0 +1,18 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #access item
+>>> b=['onion','potato','ginger','cucumber',1,3.1416]
+>>> b
+['onion', 'potato', 'ginger', 'cucumber', 1, 3.1416]
+>>> #index positions
+>>> b[0]
+'onion'
+>>> b[2]
+'ginger'
+>>> b[1:5]
+['potato', 'ginger', 'cucumber', 1]
+>>> b[:5]
+['onion', 'potato', 'ginger', 'cucumber', 1]
+>>> b[2:]
+['ginger', 'cucumber', 1, 3.1416]
+>>> 
