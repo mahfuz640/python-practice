0aca61f3bcf36e1f829422588c6434b78f4c4cba
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-02 22:16:20 +0600

    list 2

---
+++ list 2 all type.py
@@ -0,0 +1,9 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #list ar item ar deta type jekono kisu hote pare karon amra akta list shob type ar data rakhte pari
+>>> b=['onion','potato','ginger','cucumber',1,3.1416]
+>>> b
+['onion', 'potato', 'ginger', 'cucumber', 1, 3.1416]
+>>> type (b)
+<class 'list'>
+>>> 
