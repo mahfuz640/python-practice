87b420658d9e28c8ed9bed5535bfb7162df997fe
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-05 14:12:07 +0600

    update list

---
+++ update list.py
@@ -0,0 +1,32 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #update list
+>>> b=['onion','potato','ginger','cucumber',1,3.1416]
+>>> b=[4]
+>>> 'rice'
+'rice'
+>>> b=[4]
+>>> 
+>>> b
+[4]
+>>> b
+[4]
+>>> b
+[4]
+>>> b
+[4]
+b
+>>> 
+>>> b
+[4]
+>>> b=['onion','potato','ginger','cucumber',1,3.1416]
+
+>>> 
+>>> v
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    v
+NameError: name 'v' is not defined
+>>> b
+['onion', 'potato', 'ginger', 'cucumber', 1, 3.1416]
+>>> 
