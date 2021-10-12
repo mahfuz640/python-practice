18d366953d3b55f41d17547da12a04b09d40e045
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-12 15:45:43 +0600

    append function

---
+++ append function.py
@@ -0,0 +1,13 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #list ar moddhe kono kisu add korte chaile amra append () function use kora hoi
+>>> b=['rice','potato','343','cucumber','finger'23486,4678]
+SyntaxError: invalid syntax
+>>>  b=['rice','potato','343','cucumber','finger'23486.255]
+ 
+SyntaxError: unexpected indent
+>>> b=['rice','potato','343','cucumber','finger',23486.255]
+>>> b.append('new')
+>>> b
+['rice', 'potato', '343', 'cucumber', 'finger', 23486.255, 'new']
+>>> 
