44cf20d95a7cf8b621759e92c36e57ca69d16da9
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-13 15:35:43 +0600

    remove item

---
+++ remove item list 1.py
@@ -0,0 +1,21 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #item remove part one
+>>> #amra kono list theke jodi kono item k remove korte chai tokhon amra  del ba remove function use korte pari.
+>>> #1st a amr a del ar use dekhbo
+>>> a=['rice','python','potato'434,'cucumber','finger',255263]
+SyntaxError: invalid syntax
+>>> a=['rice','python','potato'434,'cucumber','finger',255263]
+SyntaxError: invalid syntax
+>>> a=['rice','python','potato'434',cucumber','finger',255263]
+SyntaxError: invalid syntax
+>>> a=['rice','python','potato','434','cucumber','finger',255263]
+>>> del b[3]
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    del b[3]
+NameError: name 'b' is not defined
+>>> del a[3]
+>>> a
+['rice', 'python', 'potato', 'cucumber', 'finger', 255263]
+>>> 
