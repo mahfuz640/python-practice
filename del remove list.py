557045cc77affcee610e3d020fc9975566e7a37d
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-13 20:19:40 +0600

    del remove

---
+++ del remove list.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #amra jodi chai remove function sara o del ar shate index use kore bujhate pari
+>>> a=[1,2,3,4,5,1,4,5,6,]
+>>> del a[-1]
+>>> a
+[1, 2, 3, 4, 5, 1, 4, 5]
+>>> 
