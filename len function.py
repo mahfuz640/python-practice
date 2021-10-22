0ad368ec9d6259cc7011594fe850607e9236b7bf
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-22 23:27:06 +0600

    len function

--- len function.py
+++ len function.py
@@ -1,9 +1,7 @@
-Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> ##amra jokhon kono string ar letter count kore jante chai tokhon amr a chaile len () function use korte pari
->>> a='Banlga'
->>> a
-'Banlga'
+>>> #amra jodi kono akta list ar moddhe koto gula variable ba item ase  ta jante chai tokhon amra len () function use korte pari
+>>> a=['ami','tumi','she']
 >>> len (a)
-6
+3
 >>> 
