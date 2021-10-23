d7ded1b052d4a837b83e394558c34a488dce8245
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-23 10:20:38 +0600

    sort fumctiom

---
+++ sort function.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #sortfuntion amra kono list ba array k shajiye ba ghuchiye deyar jonno use kore thaki
+>>> a=[8,3,5,1,6,2,9,7,0,4]
+>>> a.sort()
+>>> a
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> 
